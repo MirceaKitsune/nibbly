5fc9b7b629845b45f5ceb2f3e93143657aa04ccd
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2020-10-04 22:31:48 +0300

    Give Nibbly a body

--- js/game.js
+++ js/game.js
@@ -13,6 +13,7 @@ const MOVER_EYELID = [["zoom", 1, [0, -0.5, 0, 0]], ["interval", 1000, [0, 4, 0,
 const MOVER_EYEBROW = [["zoom", 1, [0, -1, 0, 0]], ["interval", 1000, [0, 0, 0, 8]]];
 const MOVER_JAW_LOWER = [["zoom", 1.5, [0, 3, 0, 0]]];
 const MOVER_JAW_UPPER = [["zoom", 1.5, [0, -1, 0, 0]]];
+const MOVER_TAIL = [["interval", 1, [0, 1, 0, 0]]];
 const MOVER_LIGHTING = [["zoom", 1.5, [0, -1.5, 0, 0]]];
 
 layer_add("throat", RECTANGLE_BODY, 0.5, MOVER_STATIC, ["svg/pet/ring.svg"]);
@@ -38,5 +39,12 @@ layer_add("uvula", RECTANGLE_BODY, -0.45, MOVER_STATIC, ["svg/pet/uvula.svg"]);
 layer_add("tongue", RECTANGLE_BODY, -0.475, MOVER_STATIC, ["svg/pet/tongue.svg"]);
 layer_add("throat", RECTANGLE_BODY, -0.45, MOVER_STATIC, ["svg/pet/throat.svg"]);
 
+layer_add("tail", RECTANGLE_BODY, 0.125, MOVER_TAIL, ["svg/pet/tail.svg"]);
+layer_add("feet", RECTANGLE_BODY, 0, MOVER_STATIC, ["svg/pet/foot_left.svg", "svg/pet/foot_right.svg"]);
+layer_add("arms", RECTANGLE_BODY, -0.2, MOVER_STATIC, ["svg/pet/arm_left.svg", "svg/pet/arm_right.svg"]);
+layer_add("hands", RECTANGLE_BODY, -0.25, MOVER_STATIC, ["svg/pet/hand_left.svg", "svg/pet/hand_right.svg"]);
+layer_add("chest", RECTANGLE_BODY, -0.25, MOVER_STATIC, ["svg/pet/chest.svg"]);
+
+layer_add("shadow", RECTANGLE_BODY, 0, MOVER_LIGHTING, ["svg/pet/shadow.svg"]);
 layer_add("lighting", RECTANGLE_BODY, -0.475, MOVER_LIGHTING, ["svg/pet/lighting.svg"]);
 layer_add("eye_glow", RECTANGLE_BODY, -0.45, MOVER_STATIC, ["svg/pet/eye_glow_left.svg", "svg/pet/eye_glow_right.svg"]);
